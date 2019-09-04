d65fc33e29745f9da27bb942b00c56c70d0f0b67
Richard Hpa <richard.hpa@bun2-206-05.local>
2019-09-05 11:47:43 +1200

    preparing plugin

---
+++ slideShowAddOn.php
@@ -0,0 +1,9 @@
+<?php
+    /**
+        * Plugin Name: Slideshow Add On
+        * Description: A customizable slideshow Plugin
+        * Version: 1.0.0
+        * Author: Richard Hpa
+    **/
+
+    require( plugin_dir_path( __FILE__ ) . 'includes/slideshow_customizer.php');
